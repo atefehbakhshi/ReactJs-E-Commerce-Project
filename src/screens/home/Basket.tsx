d35390c30ff3d6d30431b16c5bbc8f29bffbdc01
Atefeh <at.bakhshi1373@gmail.com>
2023-03-04 09:24:11 +0330

    feat: basket page

--- src/screens/home/Basket.tsx
+++ src/screens/home/Basket.tsx
@@ -1,5 +1,49 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../../components/buttons";
+import { BasketTable } from "../../components/tables";
+
+const selectedList = [
+  {
+    id: 1,
+    title: "کفش پاشنه بلند",
+    price: 120,
+    quantity: 2,
+  },
+  {
+    id: 2,
+    title: "عینک زنانه",
+    price: 120,
+    quantity: 2,
+  },
+  {
+    id: 3,
+    title: "تیشرت",
+    price: 120,
+    quantity: 2,
+  },
+  {
+    id: 3,
+    title: "تیشرت",
+    price: 120,
+    quantity: 2,
+  },
+];
 
 export const Basket = () => {
-  return <div>Basket</div>;
+  return (
+    <div className="p-3 max-w-xl mx-auto">
+      <h1 className="font-bold text-xl mb-4">سبد خرید</h1>
+      <BasketTable selectedList={selectedList} />
+      <div className="flex justify-between items-center gap-8 mt-4">
+        <p>
+          <span className="font-bold">جمع:</span> 256 تومان
+        </p>
+        <Link to="/checkout">
+          <Button bg="bg-[#ade5ad]" hover="hover:bg-[#bdeabd]">
+            نهایی کردن سبد خرید
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
