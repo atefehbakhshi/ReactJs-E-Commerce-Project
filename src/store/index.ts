7a51ae104ad8d806bf44a5dbd93bdcb6b6f8cbd9
Atefeh <at.bakhshi1373@gmail.com>
2023-03-15 15:39:49 +0330

    feat: add orderSlice to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import dataSlice from "./slices/data-slice";
 import modalSlice from "./slices/modal-slice";
+import orderSlice from "./slices/order-slice";
 
 const store = configureStore({
   reducer: {
     data: dataSlice,
     modal: modalSlice,
+    order: orderSlice,
   },
 });
 
