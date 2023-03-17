7b74a5f18a1eddf5ef6b8b05677319ad8186333f
Atefeh <at.bakhshi1373@gmail.com>
2023-03-17 13:23:07 +0330

    feat: sub header for filtering products

--- src/components/sub-header/index.tsx
+++ src/components/sub-header/index.tsx
@@ -1,29 +1,76 @@
+import { Icon } from "@iconify/react";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { getFilterList } from "../../store/slices/category-slice";
+
 const SubHeader = ({ text }) => {
+  const dispatch = useDispatch();
+  const [priceFilter, setPriceFilter] = useState(null);
+  const [dateFilter, setDateFilter] = useState(null);
+
+  useEffect(() => {
+    dispatch(getFilterList({ price: priceFilter, date: dateFilter }));
+  }, [priceFilter, dateFilter]);
+
   return (
     <div>
       <header className="flex justify-center mb-3 font-semibold sub-header">
         <h1>{text}</h1>
       </header>
-      <div className="flex justify-between bg-red-100 w-3/4 py-2 px-1 md:px-2 rounded mx-auto max-w-xl">
-        <select name="size" className="w-[30%]">
-          <option value="s">سایز</option>
-          <option value="s">S</option>
-          <option value="s">S</option>
-          <option value="s">S</option>
-          <option value="s">S</option>
+
+      <div className="flex gap-4 py-2 px-1">
+        <select
+          className="px-4 max-w-[10rem] hover:shadow-custom"
+          onChange={(e) => {
+            setPriceFilter(e.target.value);
+            setDateFilter(null);
+          }}
+          // do not display which option selected
+          value={""}
+        >
+          <option className="hidden">قیمت</option>
+          <option value="desc">بیشترین قیمت</option>
+          <option value="asc">کمترین قیمت</option>
         </select>
-        <select name="model" className="w-[30%]">
-          <option value="s">مدل</option>
-          <option value="s">S</option>
-          <option value="s">S</option>
-          <option value="s">S</option>
-          <option value="s">S</option>
+        <select
+          className="px-4 max-w-[10rem] hover:shadow-custom"
+          onChange={(e) => {
+            setDateFilter(e.target.value);
+            setPriceFilter(null);
+          }}
+          // do not display which option selected
+          value={""}
+        >
+          <option className="hidden">تاریخ</option>
+          <option value="desc">جدیدترین</option>
+          <option value="asc">قدیمی ترین</option>
         </select>
-        <select name="price" className="w-[30%]">
-          <option value="s">قیمت</option>
-          <option value="s">بیشترین</option>
-          <option value="s">کمترین</option>
-        </select>
+      </div>
+      <div className="flex items-center gap-2 px-4 mt-2">
+        {priceFilter && (
+          <>
+            <Icon
+              icon="carbon:close-filled"
+              width="16"
+              color="#626262"
+              onClick={() => setPriceFilter(null)}
+              className="cursor-pointer"
+            />
+            <p>{priceFilter === "asc" ? "کمترین قیمت" : "بیشترین قیمت"}</p>
+          </>
+        )}
+        {dateFilter && (
+          <>
+            <Icon
+              icon="carbon:close-filled"
+              width="16"
+              color="#626262"
+              onClick={() => setDateFilter(null)}
+              className="cursor-pointer"
+            />
+            <p>{dateFilter === "asc" ? "قدیمی ترین" : "جدید ترین"}</p>
+          </>
+        )}
       </div>
     </div>
   );
