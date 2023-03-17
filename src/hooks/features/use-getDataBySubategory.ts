eb41245e971fb8d72a0c026a23ef3653a5b380e8
Atefeh <at.bakhshi1373@gmail.com>
2023-03-17 13:24:24 +0330

    feat: modify getting data hook for filtering

--- src/hooks/features/use-getDataBySubategory.ts
+++ src/hooks/features/use-getDataBySubategory.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { fetchDataBySubcategory } from "../../api/services";
+import { useSelector } from "react-redux";
+import { fetchDataBySubcategory, fetchFiltredData } from "../../api/services";
 import { DATA_ON_PRODUCTS_PAGE } from "../../constants";
 
 const getData = async (id: number, page: number, limit: number) => {
@@ -10,16 +11,55 @@ const getData = async (id: number, page: number, limit: number) => {
   };
 };
 
+const getFilterDate = async (
+  id: number,
+  page: number,
+  limit: number,
+  filterType: string,
+  text: string
+) => {
+  const res = await fetchFiltredData(id, page, limit, filterType, text);
+  return {
+    data: res.data,
+    count: res.headers["x-total-count"],
+  };
+};
+
 export const useGetDataBySubcategory = (subCategoryId, page) => {
   const [list, setList] = useState([]);
   const [count, setCount] = useState(0);
+  const { filterList } = useSelector((state) => state.category);
 
   useEffect(() => {
-    getData(subCategoryId, page, DATA_ON_PRODUCTS_PAGE).then((res) => {
-      setList(res.data);
-      setCount(res.count);
-    });
-  }, [page]);
+    if (filterList.price && !filterList.date) {
+      getFilterDate(
+        subCategoryId,
+        page,
+        DATA_ON_PRODUCTS_PAGE,
+        "price",
+        filterList.price
+      ).then((res) => {
+        setList(res.data);
+        setCount(res.count);
+      });
+    } else if (!filterList.price && filterList.date) {
+      getFilterDate(
+        subCategoryId,
+        page,
+        DATA_ON_PRODUCTS_PAGE,
+        "createdAt",
+        filterList.date
+      ).then((res) => {
+        setList(res.data);
+        setCount(res.count);
+      });
+    } else {
+      getData(subCategoryId, page, DATA_ON_PRODUCTS_PAGE).then((res) => {
+        setList(res.data);
+        setCount(res.count);
+      });
+    }
+  }, [page, filterList]);
 
   return [list, DATA_ON_PRODUCTS_PAGE, count];
 };
