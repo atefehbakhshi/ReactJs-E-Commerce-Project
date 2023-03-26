dbf62f15bdbec24a3ccab953b1ca133db1fd6004
Atefeh <at.bakhshi1373@gmail.com>
2023-03-26 16:39:29 +0430

    feat: error page

--- src/screens/home/Error.tsx
+++ src/screens/home/Error.tsx
@@ -1,5 +1,36 @@
+import cloud from "/img/error/cloud.gif";
+import { Link } from "react-router-dom";
+
 export const Error = () => {
   return (
-    <div>Error</div>
-  )
-}
+    <div className="flex flex-col gap-4 justify-center items-center min-h-[75vh]">
+      <img src={cloud} alt="rainy-cloud" />
+      <p className="text-base font-bold">متاسفانه مشکلی رخ داده است .</p>
+      <p className="text-gray-500 text-xs">
+        شاید این صفحات برای شما جذاب باشند .
+      </p>
+      <div className="flex gap-2">
+        <Link
+          to="/products/clothes"
+          className="text-[#41c1c6] text-base cursor-pointer"
+        >
+          لباس
+        </Link>
+        <div className="border-x border-[#afafaf50] px-2">
+          <Link
+            to="/products/shoes"
+            className="text-[#41c1c6] text-base  cursor-pointer"
+          >
+            کفش
+          </Link>
+        </div>
+        <Link
+          to="/products/watch"
+          className="text-[#41c1c6] text-base  cursor-pointer"
+        >
+          ساعت
+        </Link>
+      </div>
+    </div>
+  );
+};
