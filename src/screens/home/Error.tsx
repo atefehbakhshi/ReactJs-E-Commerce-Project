d7d22982129c79400b1888d00c199bfd92ff1451
Atefeh <at.bakhshi1373@gmail.com>
2023-03-31 07:26:05 +0430

    style: change ui of error page

--- src/screens/home/Error.tsx
+++ src/screens/home/Error.tsx
@@ -6,29 +6,9 @@ export const Error = () => {
     <div className="flex flex-col gap-4 justify-center items-center min-h-[75vh]">
       <img src={cloud} alt="rainy-cloud" className="max-w-[30rem] mx-auto" />
       <p className="text-base font-bold">متاسفانه مشکلی رخ داده است .</p>
-      <p className="text-gray-500 text-xs">
-        شاید این صفحات برای شما جذاب باشند .
-      </p>
       <div className="flex gap-2">
-        <Link
-          to="/products/clothes"
-          className="text-[#41c1c6] text-base cursor-pointer"
-        >
-          لباس
-        </Link>
-        <div className="border-x border-[#afafaf50] px-2">
-          <Link
-            to="/products/shoes"
-            className="text-[#41c1c6] text-base  cursor-pointer"
-          >
-            کفش
-          </Link>
-        </div>
-        <Link
-          to="/products/watch"
-          className="text-[#41c1c6] text-base  cursor-pointer"
-        >
-          ساعت
+        <Link to="/" className="text-[#41c1c6] text-base cursor-pointer">
+          بازگشت به سایت
         </Link>
       </div>
     </div>
