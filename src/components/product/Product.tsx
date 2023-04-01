ed4a5f2a2310297073b16514595c84b4507847c3
Atefeh <at.bakhshi1373@gmail.com>
2023-04-01 08:43:27 +0430

    style:rotate warn message which show the product quantity is 0

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -5,12 +5,11 @@ import { ProductParams } from "../../type/interface";
 const Product: FC<ProductParams> = ({ img, title, price, id, quantity }) => {
   return (
     <Link to={`/products/${id}`}>
-      <div className="cart-shadow cursor-pointer hover:scale-105 md:max-w-sm relative">
-        {quantity === 0 && (
-          <p className="text-red-400 font-light text-xs absolute top-6 left-0 rotate-[-45deg]">
-            اتمام موجودی
-          </p>
-        )}
+      <div
+        className={`cart-shadow cursor-pointer hover:scale-105 md:max-w-sm relative ${
+          quantity === 0 ? "finished-product" : ""
+        }`}
+      >
         <div className="flex justify-center bg-[#bfbfbf50] p-3 h-[10rem] md:h-[14rem]">
           <img
             src={`http://localhost:3002/files/${img}`}
